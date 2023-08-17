0ef11975aade263b5c75f855cc725ecbceed4944
Tuziking <108811704+Tuziking@users.noreply.github.com>
2023-08-17 11:27:57 +0800

    点击屏幕释放烟花的Android版本，受移动端的配置影响，可能无法展示所有功能

--- app/src/main/assets/web/index.js
+++ app/src/main/assets/web/index.js
@@ -2,6 +2,10 @@ window.onload = function () {
     // 元素获取
     const canvas = document.querySelector("canvas")
     const ctx = canvas.getContext("2d");
+    const canvasIn = document.createElement('canvas');
+    canvasIn.width = window.innerWidth;
+    canvasIn.height = window.innerHeight;
+    const inCtx = canvasIn.getContext("2d")
     // 设定画布大小
     function resizeCanvas() {
         canvas.width = window.innerWidth
@@ -17,20 +21,46 @@ window.onload = function () {
     }
     // 烟花
     let fireworks = [];
+    let fireworks2 = [];
     setInterval(() => {
-        // 可以多调用几次用来增加烟花的数量
-        addFires(Math.random() * canvas.width, Math.random() * canvas.height)
-        addFires(Math.random() * canvas.width, Math.random() * canvas.height)
-    }, 500)
+        //可以多调用几次用来增加烟花的数量
+        addFires2(Math.random() * canvas.width, Math.random() * canvas.height, 1);
+        // addFires2(Math.random() * canvas.width, Math.random() * canvas.height);
+    }, 1000);
     // 获取鼠标点击位置
     function clickSite(e) {
         // 获取当前鼠标的坐标
         let x = e.clientX;
         let y = e.clientY;
         // 绘制
-        addFires(x, y);
+        // addFires(x, y);
+        addFires2(x,y,4);
+        // addFires4(x,y);
     }
     document.addEventListener('click', clickSite);
+
+    function addFires2(x,y,type){
+        // 爆炸扩散的半径
+        // 定制颜色，这是通过色域方式定义的
+        let hue = Math.random() * 360;
+        let hueVariance = 60;
+        function setColors(firework) {
+            firework.hue = Math.floor(Math.random() * ((hue + hueVariance) - (hue - hueVariance))) + (hue - hueVariance);
+            firework.brightness = Math.floor(Math.random() * 21) + 30;
+            firework.alpha = (Math.floor(Math.random() * 60) + 40) / 100;
+        }
+
+        let firework = {};
+        firework.type = type;
+        firework.x = x;
+        firework.y = canvasIn.height;
+        firework.end = y;
+        firework.size = 2;
+        firework.speed = Math.random() * 5 + .4;
+        setColors(firework)
+        fireworks2.push(firework)
+    }
+
     // 将单个烟花保存到烟花数组中
     function addFires(x, y) {
         // 烟花爆开的粒子数量
@@ -61,7 +91,32 @@ window.onload = function () {
             // 设置颜色
             setColors(firework)
             // 加入数组
-            fireworks.push(firework)
+            fireworks.push(firework);
+        }
+    }
+    function drawFires2(){
+        clearCanvas();
+        for (let i = 0; i < fireworks2.length; i++) {
+            // 渲染出当前数据
+            let firework = fireworks2[i];
+            // 下面是点数学题
+            let moveY = firework.speed;
+            firework.y -= moveY;
+            // 如果透明度小于0就删除这个粒子
+            if (firework.end >= firework.y) {
+                fireworks2.splice(i, 1);
+                addFires(firework.x, firework.end)
+
+                // 跳过这次循环，不进行绘制
+                continue;
+            }
+            // 开始路径
+            ctx.beginPath();
+            ctx.arc(firework.x, firework.y, firework.size, Math.PI * 2, false);
+            // 结束
+            ctx.closePath();
+            ctx.fillStyle = 'hsla(' + firework.hue + ',100%,' + firework.brightness + '%,' + firework.alpha + ')';
+            ctx.fill();
         }
     }
     // 绘制烟花
@@ -94,7 +149,10 @@ window.onload = function () {
             ctx.fillStyle = 'hsla(' + firework.hue + ',100%,' + firework.brightness + '%,' + firework.alpha + ')';
             ctx.fill();
         }
+
+
     }
+
     // 渲染,更新粒子的信息
     function tick() {
         // 设置拖影
@@ -104,6 +162,7 @@ window.onload = function () {
         ctx.globalCompositeOperation = 'lighter';
         // 更新画布
         drawFires();
+        drawFires2();
         requestAnimationFrame(tick);
     }
     tick()
